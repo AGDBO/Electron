97dd07e32ff9ff6a0e1c2596021d1e8ccce2f073
Annie Garcia Dutaitre <agarciadutaitre@gmail.com>
2023-06-19 08:46:26 +0200

    change readme

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 Expérience de code avec Electron pour apprendre comment transférer MFSS 
 dans un projet Electron ...
+creation application Electron
+
+-creation d'un package.json
+(entry point doit être main.js & author et description peuvent prendre n’importe quelle valeur, mais sont nécessaires pour le packaging de l'application.)
+
+npm init 
+
+-installation Electron 
+
+npm install --save-dev electron
+
+Ajouter dans package.json
+{
+  "scripts": {
+    "start": "electron ."
+  }
+}
+
+-Lancer l'application Electron
+
+npm start
+
+
+Le point d’entrée de toute application Electron est son script main.
+
+- Installer Forge pour créer l'exe et l'installateur
+Ajoutez Electron Forge en tant que dépendance de développement et utilisez la commande import pour initialiser le squelette de base de Forge.
+
+npm install --save-dev @electron-forge/cli
+
+npx electron-forge import
+
+
+
+✔ Checking your system
+✔ Initializing Git Repository
+✔ Writing modified package.json file
+✔ Installing dependencies
+✔ Writing modified package.json file
+✔ Fixing .gitignore
+
+We have ATTEMPTED to convert your app to be in a format that electron-forge understands.
+
+Thanks for using "electron-forge"!!!
+
+
+- Lancer la création d'une distribution 
+
+npm run make
+
+Electron Forge crée un dossier out où se trouvera vos fichiers de distribution
--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-// Définir le chemin de l'icône
-const iconPath = path.join(__dirname, '.\\src\\asset\\images\\logo6b.png');
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  // Définir le chemin de l'icône
+  const iconPath = path.join(__dirname, ".\\src\\asset\\images\\logo6b.png");
 
-// Définir l'icône de la fenêtre
-win.setIcon(iconPath);
+  // Définir l'icône de la fenêtre
+  win.setIcon(iconPath);
 
-  win.loadFile('index.html')
+  const appPath = path.join(__dirname, ".\\src\\index.html");
+  win.loadFile(appPath);
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
